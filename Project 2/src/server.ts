0e715929cf38e1ff9a7689da8671156753b0f4e3
abhinavjha98 <abhinavjha98ald@gmail.com>
2020-05-31 17:47:03 +0530

    Uploaded the project 2

---
+++ Project 2/src/server.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import { isUri } from 'valid-url';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    const { image_url: imageUrl } = req.query;
+    if (!imageUrl || !isUri(imageUrl)) {
+      return res.status(400).send({ auth: false, message: 'Image url is missing or malformed' });
+    }
+
+    const filteredPath = await filterImageFromURL(imageUrl);
+
+    res.sendFile(filteredPath, {}, () => deleteLocalFiles([filteredPath]));
+  });
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
